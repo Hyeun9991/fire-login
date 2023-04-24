365cda12a218746f55593b2c869181d46c74a0f7
Hyeun9991 <hyeun9991@gmail.com>
2023-04-24 17:41:54 +0900

    Add User slice Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import useReducer from './userSlice';
 
 export const store = configureStore({
   reducer: {
+    user: useReducer,
   },
 });
 
