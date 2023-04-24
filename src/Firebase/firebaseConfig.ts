cff2190cdc3b2d14f141c64ecea08bd6f82e920c
Hyeun9991 <hyeun9991@gmail.com>
2023-04-24 17:34:07 +0900

    Firebase Initial Set up , Integrate Firebase Provider

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAcutPlJl6nki15DdwdQzVecSYYuGbJzGE',
+  authDomain: 'fire-login-cd8fc.firebaseapp.com',
+  projectId: 'fire-login-cd8fc',
+  storageBucket: 'fire-login-cd8fc.appspot.com',
+  messagingSenderId: '31063259613',
+  appId: '1:31063259613:web:230006fb4c963efab31e34',
+  measurementId: 'G-DTL97Z93BY',
+};
